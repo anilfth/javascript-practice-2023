948c15278a96d975509452f3d0ff4aed33ebfa87
Anil <anilfth2001@gmail.com>
2023-09-10 11:53:41 +0530

    secoond commit

--- value-variable/day1.js
+++ value-variable/day1.js
@@ -1,2 +1,14 @@
 var myName = "Anil"
 console.log(myName);
+var _myName = "Anil"
+console.log(_myName)
+ 
+// var 1myName = "anil"  // 
+// console.log (1myName)
+
+ var $myName = "Anil"
+ console.log($myName)
+
+ var _my__Name = "Anil"
+ console.log(_my__Name)
+
