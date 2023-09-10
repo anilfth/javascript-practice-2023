f119dad8037645815eb86a948d39f1a0b1db05cc
Anil <anilfth2001@gmail.com>
2023-09-10 12:33:26 +0530

    third commit

---
+++ data-types/day1.js
@@ -0,0 +1,43 @@
+// there a six primiteve data types in javascript 
+//  boolean  ==> typeofinstance === "boolean"
+ 
+var myName = "anil"
+console.log(myName)
+//  types of variable
+console.log(typeof(myName))
+
+var myAge = 26
+console.log(myAge)
+//  types od variable
+console.log(typeof(myAge))
+var myLpu = true
+
+console.log(myLpu)
+//  types of variable 
+console.log(typeof(myLpu))
+
+
+//  challenges time 2 
+  ///   1) 10 + "20"
+  console.log(10 + "20") // it is called concatination   bcz here  we are adding  number and string so output will be 1020
+  
+   // 2* 9 - "5"
+ console.log(9 - "5") // o/p = 4 its bug
+//   3*  "java" + "script" 
+ console.log("java"  + "script") /// adding a two strings
+   
+//   4*  "" + "" 
+ console.log(" "  + " ")
+
+ // 5* " " + 0
+ console.log( " " + 0)
+  
+//    6*  "Anl" - "LPU"
+console.log("Anil" - "Lpu") // nan -> not a number
+// 7*  true + true
+ console.log (true + true)
+ console.log(true + false)
+ console.log(false + true)
+ console.log(false - false)
+ 
+
