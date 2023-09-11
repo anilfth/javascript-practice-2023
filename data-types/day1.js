4359cb98ae08c419f20c8316ab860528a42dfc5f
Anil <anilfth2001@gmail.com>
2023-09-11 14:25:30 +0530

    intial commit

--- data-types/day1.js
+++ data-types/day1.js
@@ -81,4 +81,103 @@ console.log( (a>b)  && (b>a) ) // here first condition i true and second condtio
     console.log(a > b || b > a) //  here first condition is false and second consition is true  so o/p will be true 
     console.log( (a > b) || (b > a) && (a > b));  //1st conditon is false 2nd is true so o/p true because of we are using or operator and second condition is false so ouptput wwill be false because w are using and operator 
 
+                      // LOGICAL Notification(!)
+                      //  IT IS CONVERT TRUE TO FALSE AND FALSE T9O  TRUE 
+       var  a  = 10
+        var b = 20 
+         console .log(! ((a > b) || (b > a)) )
+
+
+//  
+// STRING CANCATINATION 
+  
+ console.log("hello world")
+  console.log("hello " + "World")
+   var myName = " Anil"
+    console.log(myName + " Nishad")
+     
+    //  EXPONENTIATION
+     
+    console.log(3**3)  // base ^ exponent 
+     console.log(9**2);
+      console.log( 100**-1)
+      //  write a program to swap  a two number 
+       var a = 10
+        var b = 20 
+        var c = b; // c =20
+        b = a // b = 10
+        a = c // a =20
+         console.log("the va,ue of a is " + a);
+          console.log("the value of b is " + b );
+
+          //  write a program swap two number withoou using third variable 
+
+           var a = 5 
+            var b = 10
+             a = a + b // a =15 
+              b = a - b  // b = 5 
+               a = a - b // a 10 
+                console.log( "the value of a " + a)
+                 console.log( "the value of the b is " + b)
+                  
+                //  write a  program to check a yera is a leap year is not 
+
+      var year = 2020;
+       if ( year % 4 === 0){
+         if (year % 100 === 0 ){
+          if(year % 400 === 0 ){
+             console.log("the year is" + year + "is a leap year");
+          }else {
+             console.log("the year is" + year + "is not leap year")
+             }
+          }
+           else{
+            console.log("the year is" + year + "is a  yaer is leap year")
+           }
+         }
+        else{
+          console.log("the year is" + year + " is not a a leap year")
+        }
+                
+
+
+          // TERNARY OPEARTOR 
+    // SYNTAX ==>   VARAIBLENAME = (CONDITION)? VALUE1:VALUE2
+       
+     var age = 17
+     console.log ((age >= 18)? "you can vote":"you can't vote")
+
+      // using switch statement find the are a of the circle tringle and recatnagel 
+       
+      var area  = "tringle"
+      var pi=3.14,r=4, l=3, b=4;
+      switch(area){
+        case 'circle':
+          console.log("the area of circle:" + (pi*r**2))
+          break;
+          case 'tringle':
+            console.log("the area of the tringle:" + (l*b)/2)
+            break
+            case 'rectangle':
+              console.log("the are of the recatngle:" + (l*b))
+              break
+              default:
+                console.log("please enter a valid data")
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
