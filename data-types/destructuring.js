60c34394c1c917ac4a98f5341413b0122dbff190
Anil <anilfth2001@gmail.com>
2023-10-01 03:02:55 +0530

    intial commit

---
+++ data-types/destructuring.js
@@ -0,0 +1,7 @@
+// arrays destructuring 
+const mybioData = ['Anil', 'Nishad', 22];
+// let myFNAme = mybioData[0];
+// let myLName = mybioData[1];
+// let myAge = mybioData[2];
+ let [myFNAme, myLName, myAge] = mybioData;
+ console.log(myAge)
