ab3efe19e84daa51a560ba01979ee8e6ef8e8eb7
Anil <anilfth2001@gmail.com>
2023-10-02 21:19:32 +0530

    intial commit

--- data-types/spreadoperator.js
+++ data-types/spreadoperator.js
@@ -4,3 +4,8 @@ function sum (x, y, z){
 const numbers =[1,2,3];
 console.log(sum(...numbers));
 console.log(sum.apply(null, numbers));
+
+ const colors = ['red', 'green', 'blue', 'black','pink'];
+ const myColors = ['red', 'green', 'blue', 'black', 'pink', 'yellow','white'];
+ const myFavColors = [...colors, 'yelow', 'white'];
+ console.log(myFavColors);
