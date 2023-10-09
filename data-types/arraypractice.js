f2da3d43b132e35252ed08ecc5a0d18bfaacd75c
Anil <anilfth2001@gmail.com>
2023-10-09 22:43:38 +0530

    intial commit

--- data-types/arraypractice.js
+++ data-types/arraypractice.js
@@ -1,2 +1,7 @@
+ // array.prototype.indexOf()
 var myFriendnames = ['vindo', 'nikhil', 'vikky', 'monkey','vikky'];
 console.log(myFriendnames.indexOf("vikky", 6));
+ 
+ // array.prototype.lastindexOf()
+  var myFriendNames = ['anil','vikky','nikhil','vikky','vindo'];
+  console.log(myFriendNames.lastIndexOf('vikky', 0));
