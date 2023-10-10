169b8059c306824a2aed36fe4462ba7c3b88faaf
Anil <anilfth2001@gmail.com>
2023-10-10 13:20:10 +0530

    intail comiit

--- data-types/arraypractice.js
+++ data-types/arraypractice.js
@@ -5,3 +5,25 @@ console.log(myFriendnames.indexOf("vikky", 6));
  // array.prototype.lastindexOf()
   var myFriendNames = ['anil','vikky','nikhil','vikky','vindo'];
   console.log(myFriendNames.lastIndexOf('vikky', 0));
+
+   // array.prototype.includes()
+    var myFriendNames1 = ['anil','vikky','nikhil','vikky','vindo'];
+    console.log(myFriendNames1.includes('anil', 4));
+
+     // array.prototype.find()
+      
+      const prices = [200, 250,300, 350, 400, 450, 500];
+      const findElem = prices.find((currval) => {
+        return currval < 400;
+      })
+       console.log(findElem);
+
+        // array.prototype.findIndex()
+         
+         console.log(prices.findIndex((currVal)=> currVal < 400));
+
+          // array.prototype.filter()
+          const newPriceTag = prices.filter((elem, index) =>{
+            return elem < 400 ;
+          }) 
+          console.log(newPriceTag)
