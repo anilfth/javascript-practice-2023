2f8453ad3c894da143fd8469ae4bbcbe9d641b35
Anil <anilfth2001@gmail.com>
2023-10-22 03:04:30 +0530

    single parameter commit

--- data-types/Arrowfun.js
+++ data-types/Arrowfun.js
@@ -7,3 +7,14 @@ greet();
  // OR
  const greet1 = () => 'Hello, Sir Good morning'
  console.log(greet1());
+
+
+ // Single Parameter 
+  const square = (x) => x * x;
+  console.log(square(4));
+   
+  // OR
+   const square1 = x => {
+    console.log(x *x);
+   }
+   square1(5)
