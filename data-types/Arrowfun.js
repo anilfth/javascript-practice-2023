2b1a29719cf64b8e1d89705075dda68e1fcb245f
Anil <anilfth2001@gmail.com>
2023-10-23 12:22:45 +0530

    javascript strig methods done

--- data-types/Arrowfun.js
+++ data-types/Arrowfun.js
@@ -46,8 +46,29 @@ greet();
         console.log(senetence.trim().length);
          
          
-        // to convert toLowerCase 
+        // to convert toLowerCase--> convert all character in a string to lowercase 
          console.log(senetence.toLowerCase());
 
-         // to conert toUperCase
+         // to convert toUperCase --> conert all character in a string to toUpperCase
           console.log(senetence.toUpperCase());
+
+
+           // charAt(index) --> returns the character at the specified index in the string
+           console.log(senetence.charAt(11));
+             
+            // substring --> returns a portion of the string starting from startindex to endindex
+            console.log(senetence.substring(0,6));
+             
+            // indexOf 
+             console.log(senetence.indexOf('n'));
+             console.log(senetence.indexOf('fun'));
+             console.log(senetence.indexOf('javascript'));
+              
+            //   split() --> splits the string into an array of the substring based on the separator 
+             console.log(senetence.split('java', 'fun'));
+              console.log(senetence.split())
+              console.log(senetence.split('javascript'));
+
+            //   trim()--> removes leading and trailing white spaces from the string
+              let name = " Lovely professionakl university is  a india largest univesity "
+               console.log(name.trim());
